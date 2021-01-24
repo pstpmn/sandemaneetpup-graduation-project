b554759abb4c2d56455b0dc89a7ee467e7aacfce
FramePSU62 <74409997+FramePSU62@users.noreply.github.com>
2021-01-25 00:37:45 +0700

    Add files via upload
    ไฟล์ show_information_success , show_information_pending, show_information_book ได้เชีืมต่อกับ มี ชื่อ นามสกุล เบอร์โทร ชั้น ฝั่ง ที่นั่ง ต้นทาง ปลายทาง หมายเลขเรือ ส่วนที่ยังไม่ได้เพิ่ม เวลาออกเรือ วันที่ออกเดินทาง เดินทาง ราคา ส่วนลด ยอดรวมชำระ

--- customer/check_information.php
+++ customer/check_information.php
@@ -7,21 +7,15 @@ session_start();
             $ticket_code = $_POST['ticket_code'];
 
             $sql = "SELECT * FROM buy_ticket AS buy_t
-            -- JOIN location AS o ON buy_t.orgin = o.location_id
-            JOIN boat_seat AS b ON buy_t.boat_seat_id = b.boat_seat_id
+            JOIN location AS i ON buy_t.destination = i.location_id
             WHERE ticket_code = '$ticket_code' ";
             $result = mysqli_query($con,$sql);
 
             if(mysqli_num_rows($result)){
                 $row = mysqli_fetch_array($result);
-                    $_SESSION["ticket_status_id"] = $row["ticket_status_id"];
-                    $_SESSION["buy_ticket_id"] = $row["buy_ticket_id"];
                     $_SESSION["ticket_code"] = $row["ticket_code"];
-                    
-                    $_SESSION["floor"] = $row["floor"];
-                    $_SESSION["boat_seat_number"] = $row["boat_seat_number"];
-                    $_SESSION["boat_seat_type"] = $row["boat_seat_type"];
-                    $_SESSION["boat_number"] = $row["boat_number"];
+                    $_SESSION["ticket_status_id"] = $row["ticket_status_id"];                    
+                    $_SESSION["destinatio"] = $row["location_name"];
                     
 
                     if($_SESSION["ticket_status_id"]== 1){
--- customer/show_information_success.php
+++ customer/show_information_success.php
@@ -2,20 +2,9 @@
 include('header.php');
 ?>
 
-  <?php session_start(); 
-    include('mysqli_connect.php');
-    $buy_ticket_id = $_SESSION['buy_ticket_id'];
+  <?php session_start();    
     $ticket_code = $_SESSION['ticket_code'];
-
-    $floor = $_SESSION['floor'];
-    $boat_seat_number = $_SESSION['boat_seat_number'];
-    $boat_seat_type = $_SESSION['boat_seat_type'];
-    $boat_number = $_SESSION['boat_number'];
-    
-
-    $sql = "SELECT * FROM buy_ticket
-    WHERE ticket_code = '$ticket_code' ";
-    $result = mysqli_query($con,$sql);
+    $destinatio = $_SESSION['destinatio'];
   ?>
 
 <style>
@@ -60,68 +49,77 @@ include('header.php');
       รหัสการซื้อตั๋ว Ticket Code : <?php echo  $ticket_code ?> <span style="color:#22E906">(ชำระแล้ว)</span></h3>
       <center>
       <form>
+<!-- select * from boat_schedule 
+where boat_number = หมายเลขเรือ AND 
+(location_id = ต้นทาง OR location_id = ปลายทาง ) 
+order by order_location ASC -->
+      <?php 
+        include('mysqli_connect.php');
+        $sql = "SELECT * FROM buy_ticket AS bt
+        INNER JOIN customer as c ON bt.customer_id = c.customer_id
+        INNER JOIN boat_seat as ba ON bt.boat_seat_id = ba.boat_seat_id
+        INNER JOIN boat as b ON ba.boat_number = b.boat_number
+        INNER JOIN location AS o ON bt.orgin = o.location_id  
+        WHERE ticket_code = '$ticket_code' ";
+        $result1 = mysqli_query($con,$sql);
       
-        <div class="box-3">
-          <b>ข้อมูลผู้โดยสาร</b><hr>
+        echo "<div class='box-3'>";
+          echo "<b>ข้อมูลผู้โดยสาร</b><hr>";
             
-            <table>
-            <tr>
+            echo "<table>";
+            echo "<tr>
               <th>ชื่อ - นามสกุล</th>
               <th>เบอร์โทรศัพท์</th>
               <th style='text-align:center'>ชั้น</th>
               <th style='text-align:center'>ฝั่ง</th>
               <th style='text-align:center'>ที่นั่ง</th>
-            </tr>
-            
-            <tr>
-              <td> <?php echo  $buy_ticket_id ?> </td>
-              <td> <?php echo  $floor ?> </td>
-              <td style="text-align:center"> <?php echo  $floor ?> </td>
-              <td style="text-align:center"> <?php echo  $boat_seat_type ?> </td>
-              <td style="text-align:center"> <?php echo  $boat_seat_number ?> </td>
-          </tr>
-
-          <tr>
-              <td>นาย ชัชพงศ์์ บวรธนสาร</td>
-              <td>098-7654321</td>
-              <td style="text-align:center">T</td>
-              <td style="text-align:center">R</td>
-              <td style="text-align:center">23</td>
-          </tr>
-          
-        </table>
-            
-        </div>
+            </tr>";
+            foreach( $result1 as $row ) {
+            echo "<tr>";
+              echo "<td>" .$row["cust_first_name"]. " " .$row["cust_last_name"]. "</td>";
+              echo "<td>" .$row["phone_number"].  "</td>";
+              echo "<td style='text-align:center'>" .$row["floor"]. "</td>";
+              echo "<td style='text-align:center'>" .$row["boat_seat_type"]. "</td>";
+              echo "<td style='text-align:center'>" .$row["boat_seat_number"]. "</td>";
+            echo "</tr>";
+          }?>
+          <?php
+            mysqli_close($con);
+          ?>
+        <?php
+        echo "</table>";
+        echo "</div>";
+        ?>
         
-      
-        <div class="box-2">
-
-          <div class="box-4">
-                <b>ข้อมูลการเดินทาง</b><hr>
-
-                <table style="overflow-x:auto;">
-                  <tr>
-                    <th class="th-2">ต้นทาง :</th>
-                    <td><?php echo  $boat_seat_type ?></td>
-                  </tr>
-                  <tr>
-                    <th class="th-2">ปลายทาง :</th>
-                    <td><?php echo  $boat_seat_number ?></td>
-                  </tr>
-                  <tr>
-                    <th class="th-2">วันที่ออกเดินทาง :</th>
-                    <td>30/12/2020</td>
-                  </tr>
-                  <tr>
-                    <th class="th-2">เวลาออกเรือ :</th>
-                    <td>08.30 น.</td>
-                  </tr>
-                  <tr>
-                    <th class="th-2">หมายเลขเรือ :</th>
-                    <td><?php echo  $boat_number ?></td>
-                  </tr>
-    
-              </table>
+      <?php 
+        echo "<div class='box-2'>";
+          echo "<div class='box-4'>";
+              echo "<b>ข้อมูลการเดินทาง</b><hr>";
+                echo "<table style='overflow-x:auto;'>";
+                  echo "<tr>";
+                    echo "<th class='th-2'>ต้นทาง :</th>";
+                    echo "<td>" .$row["location_name"]. "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                    echo "<th class='th-2'>ปลายทาง :</th>";
+                    echo "<td> $destinatio </td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                    echo "<th class='th-2'>หมายเลขเรือ :</th>";
+                    echo "<td>" .$row["boat_number"]. "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                    echo "<th class='th-2'>เวลาออกเรือ :</th>";
+                    echo "<td>" .$row["boat_number"]. "น.</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                    echo "<th class='th-2'>วันที่ออกเดินทาง :</th>";
+                    echo "<td>" .$row["boat_number"]. "</td>";
+                  echo "</tr>";
+                  
+                  
+              echo "</table>";
+              ?>
 
             </div>
 
