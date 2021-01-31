b4d086b093a35b6b6306e33b8128dd3381c4ec6c
FramePSU62 <74409997+FramePSU62@users.noreply.github.com>
2021-01-31 17:41:17 +0700

    Add files via upload
    check_in check_out แบบกลุ่ม แบบ 1ที่นั่ง

--- customer/up_check_in_out.php
+++ customer/up_check_in_out.php
@@ -5,32 +5,43 @@
 
     $date = date('Y-m-d H:i:s');
     $ticket_code = $_POST['ticket_code'];
+    $phone = $_POST['phone'];
     
-    $sql = "SELECT * FROM buy_ticket where ticket_code = '$ticket_code'";
+    $sql = "SELECT * FROM buy_ticket
+    WHERE  ticket_code = '$ticket_code' ";
     $result = mysqli_query($con,$sql) or die ("Error in query: $sql " . mysqli_error());
     $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+    $sql1 = "SELECT * FROM buy_ticket as bt
+    INNER JOIN customer as c ON bt.customer_id = c.customer_id 
+    WHERE  ticket_code = '$ticket_code' AND phone_number = '$phone' ";
+    $result1 = mysqli_query($con,$sql1) or die ("Error in query: $sql1 " . mysqli_error());
+    $data1 = mysqli_fetch_all($result1,MYSQLI_ASSOC);
 ?>
 
+<!-- check_in - check_out group-->
+
+<!-- check_in group -->
 <?php
     if (isset($_POST["check_in_group"])) {
         if(count($data) >= 1){
             if(count($data) >= 2){
                 if($data[0]['ticket_status_id'] == 1){
                     if($data[0]["check_in"] == null){
-                        $sql1 = "UPDATE buy_ticket SET 
+                        $sql2 = "UPDATE buy_ticket SET 
                         check_in = '$date'
                         WHERE ticket_code = '$ticket_code'  AND ticket_status_id = '1' ";
-                        $result1 = mysqli_query($con,$sql1) or die ("Error in query: $sql1 " . mysqli_error());
+                        $result2 = mysqli_query($con,$sql2) or die ("Error in query: $sql2 " . mysqli_error());
                         
                         echo "<script type='text/javascript'>";
                         echo "window.location = 'check_in_out.php'; ";
-                        echo "alert('$ticket_code Check In เรียบร้อย ');";
+                        echo "alert('รหัส $ticket_code Check In เรียบร้อย ');";
                         echo "</script>";    
                     }
                     else{
                         echo "<script type='text/javascript'>";
                         echo "window.location = 'check_in_out.php'; ";
-                        echo "alert('$ticket_code มีการ Check In แล้ว');";
+                        echo "alert('รหัส $ticket_code มีการ Check In แล้ว');";
                         echo "</script>";
                     }
                 }
@@ -44,7 +55,7 @@
             else{
                 echo "<script type='text/javascript'>";
                 echo "window.location = 'check_in_out_individual.php'; ";
-                echo "alert('$ticket_code รหัสนี้มีจำนวนที่นั่ง แค่ 1 ที่นั่ง ควร Check In ที่นั่งเดียว');";
+                echo "alert('$ticket_code รหัสนี้มีจำนวนที่นั่ง แค่ 1 ที่นั่ง ควร Check In แบบ 1 ที่นั่ง');";
                 echo "</script>";
             }       
         }
@@ -58,35 +69,27 @@
     }
 ?>
 
-
+<!-- check_out group -->
 <?php 
     if (isset($_POST["check_out_group"])){
         if(count($data) >= 1){
             if(count($data) >= 2 ){
                 if($data[0]['ticket_status_id'] == 1){
-                    if($data[0]["check_in"] != null){
-                        if($data[0]["check_out"] == null){
-                            $sql1 = "UPDATE buy_ticket SET 
-                            check_out = '$date'
-                            WHERE ticket_code = '$ticket_code'  AND ticket_status_id = '1' ";
-                            $result1 = mysqli_query($con,$sql1) or die ("Error in query: $sql1 " . mysqli_error());
-                            
-                            echo "<script type='text/javascript'>";
-                            echo "window.location = 'check_in_out.php'; ";
-                            echo "alert('$ticket_code Check Out เรียบร้อย ');";
-                            echo "</script>";
-                        }
-                        else{
-                            echo "<script type='text/javascript'>";
-                            echo "window.location = 'check_in_out.php'; ";
-                            echo "alert('$ticket_code มีการ Check Out แล้ว');";
-                            echo "</script>";
-                        }
+                    if($data[0]["check_out"] == null){
+                        $sql2 = "UPDATE buy_ticket SET 
+                        check_out = '$date'
+                        WHERE ticket_code = '$ticket_code'  AND ticket_status_id = '1' ";
+                        $result2 = mysqli_query($con,$sql2) or die ("Error in query: $sql2 " . mysqli_error());
+                        
+                        echo "<script type='text/javascript'>";
+                        echo "window.location = 'check_in_out.php'; ";
+                        echo "alert('รหัส $ticket_code Check Out เรียบร้อย ');";
+                        echo "</script>";
                     }
                     else{
                         echo "<script type='text/javascript'>";
                         echo "window.location = 'check_in_out.php'; ";
-                        echo "alert('ยังไม่มีการ Check In กรุณา Check In ก่อน');";
+                        echo "alert('รหัส $ticket_code มีการ Check Out แล้ว');";
                         echo "</script>";
                     }
                 }
@@ -100,7 +103,7 @@
             else{
                 echo "<script type='text/javascript'>";
                 echo "window.location = 'check_in_out_individual.php'; ";
-                echo "alert('$ticket_code รหัสนี้มีจำนวนที่นั่ง แค่ 1 ที่นั่ง ควร Check In ที่นั่งเดียว');";
+                echo "alert('$ticket_code รหัสนี้มีจำนวนที่นั่ง แค่ 1 ที่นั่ง ควร Check In แบบ 1 ที่นั่ง');";
                 echo "</script>";
             }
         }
@@ -112,3 +115,90 @@
         }
     }
 ?>
+
+<!-- check_in - check_out  group-->
+
+<!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+
+<!-- check_in - check_out  individual-->
+
+<!-- check_in individual -->
+<?php 
+    if (isset($_POST["check_in_individual"])){
+        if(count($data1) >= 1){
+            if($data1[0]['ticket_status_id'] == 1){
+                if($data1[0]["check_in"] == null){
+                    $sql3 = "UPDATE buy_ticket as bt
+                    INNER JOIN customer as c ON bt.customer_id = c.customer_id
+                    SET check_in = '$date'
+                    WHERE ticket_code = '$ticket_code'  AND ticket_status_id = '1' AND  phone_number = '$phone' ";
+                    $result3 = mysqli_query($con,$sql3) or die ("Error in query: $sql3 " . mysqli_error());
+                    
+                    echo "<script type='text/javascript'>";
+                    echo "window.location = 'check_in_out.php'; ";
+                    echo "alert('รหัส $ticket_code Check In เรียบร้อย ');";
+                    echo "</script>"; 
+                }
+                else{
+                    echo "<script type='text/javascript'>";
+                    echo "window.location = 'check_in_out.php'; ";
+                    echo "alert('รหัส $ticket_code มีการ Check In แล้ว');";
+                    echo "</script>";
+                    }
+            }
+            else{
+                echo "<script type='text/javascript'>";
+                echo "window.location = 'status.php'; ";
+                echo "alert(".$data[0]['ticket_status_id']."สถานะของคุณยังไม่ผ่านการตรวจ โปรดตรวจสอบสถานะด้วย !!!');";
+                echo "</script>";
+                }
+        }
+        else{
+            echo "<script type='text/javascript'>";
+            echo "window.location = 'check_in_out.php'; ";
+            echo "alert('รหัสหรือเบอร์โทรศัพท์ไม่ถูกต้อง');";
+            echo "</script>";
+        }
+    }
+?>
+
+<!-- check_out individual -->
+<?php 
+    if (isset($_POST["check_out_individual"])){
+        if(count($data1) >= 1){
+            if($data1[0]['ticket_status_id'] == 1){
+                if($data1[0]["check_out"] == null){
+                    $sql3 = "UPDATE buy_ticket as bt
+                    INNER JOIN customer as c ON bt.customer_id = c.customer_id
+                    SET check_out = '$date'
+                    WHERE ticket_code = '$ticket_code'  AND ticket_status_id = '1' AND  phone_number = '$phone' ";
+                    $result3 = mysqli_query($con,$sql3) or die ("Error in query: $sql3 " . mysqli_error());
+                    
+                    echo "<script type='text/javascript'>";
+                    echo "window.location = 'check_in_out.php'; ";
+                    echo "alert('รหัส $ticket_code Check Out เรียบร้อย ');";
+                    echo "</script>"; 
+                }
+                else{
+                    echo "<script type='text/javascript'>";
+                    echo "window.location = 'check_in_out.php'; ";
+                    echo "alert('รหัส $ticket_code มีการ Check Out แล้ว');";
+                    echo "</script>";
+                    }
+            }
+            else{
+                echo "<script type='text/javascript'>";
+                echo "window.location = 'status.php'; ";
+                echo "alert(".$data[0]['ticket_status_id']."สถานะของคุณยังไม่ผ่านการตรวจ โปรดตรวจสอบสถานะด้วย !!!');";
+                echo "</script>";
+                }
+        }
+        else{
+            echo "<script type='text/javascript'>";
+            echo "window.location = 'check_in_out.php'; ";
+            echo "alert('รหัสหรือเบอร์โทรศัพท์ไม่ถูกต้อง');";
+            echo "</script>";
+        }
+    }
+?>
+<!-- check_in - check_out individual-->
