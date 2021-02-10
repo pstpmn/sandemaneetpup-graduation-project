1c371b3303369b3f25b034be850316a220c4a9b0
FramePSU62 <74409997+FramePSU62@users.noreply.github.com>
2021-02-10 20:49:28 +0700

    Add files via upload
    ทำส่วน ตรวจสถานะ ให้แสดงได้ละ เหลือ อัพสลิป เช็คอิน-เช็คเอาท์ จองตั๋ว

--- customer/status.php
+++ customer/status.php
@@ -7,7 +7,7 @@
         <center>
         <div class="form-group"> 
             <form action="check_information.php" method="POST"  name="form1" id="form1"> 
-                <input type="text" class="form-control" id="ticket_code"  name="ticket_code" placeholder="รหัสการซื้อตั๋ว">
+                <input type="text" class="form-control" id="id_code" name="ticket_book"  placeholder="รหัสจองตั๋ว">
 
                 <button type="submit" class="btn btn-primary">ตกลง</button></a>
                 <a href='index.php'><button type="button" class="btn btn-danger">กลับไปยังหน้าแรก</button></a>
